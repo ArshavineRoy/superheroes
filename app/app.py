65057a4ad28cc9030e0c8c683cf8355e301aa2c9
Arshavine Waema <63721223+ArshavineRoy@users.noreply.github.com>
2023-10-01 20:31:19 +0300

    create restx Namespace and resolve flask app errors

--- app/app.py
+++ app/app.py
@@ -4,12 +4,14 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-from flask import Flask, make_response
+from flask import Flask
 from flask_migrate import Migrate
 from models.db_config import db
 from models.hero import Hero
 from models.power import Power
 from models.hero_power import HeroPower
+from flask_restx import Api, Resource, Namespace, fields
+from flask_marshmallow import Marshmallow
 
 app = Flask(
     __name__,
@@ -24,11 +26,19 @@ app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
+ma = Marshmallow(app)
 
+api = Api()
+api.init_app(app)
 
-@app.route('/')
-def home():
-    return ''
+ns = Namespace("api")
+api.add_namespace(ns)
+
+@ns.route('/')
+class Heroes(Resource):
+    def get(self):
+        return {"message" : "Welcome Home"}, 200
+
 
 
 if __name__ == '__main__':

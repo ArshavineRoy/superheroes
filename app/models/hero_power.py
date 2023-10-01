60ed9ae7ee6de570e925135e7a2828c63bd95c73
Arshavine Waema <63721223+ArshavineRoy@users.noreply.github.com>
2023-10-01 17:41:54 +0300

    add HeroPower model with strength validation

--- app/models/hero_power.py
+++ app/models/hero_power.py
@@ -3,3 +3,15 @@ from db_config import db
 class HeroPower(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+
+    def validate_strength(strength):
+        allowed_strengths = ['Strong', 'Weak', 'Average']
+        if strength not in allowed_strengths:
+            return False
+        return True

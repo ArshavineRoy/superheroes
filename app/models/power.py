0b02fd8ece6137149c186fc5b431129a85d609d9
Arshavine Waema <63721223+ArshavineRoy@users.noreply.github.com>
2023-10-01 17:35:22 +0300

    add Power model

--- app/models/power.py
+++ app/models/power.py
@@ -4,3 +4,7 @@ from db_config import db
 class Power(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String(20))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
